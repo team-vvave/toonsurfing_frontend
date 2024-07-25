f3ccafd32e752c4ddd43abf5f956e05857ee85a1
hypoxisaurea <flickerlight14@naver.com>
2024-07-25 20:51:10 +0900

    [Feat] Add Header

--- src/components/ChatBackground.jsx
+++ src/components/ChatBackground.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import logo from "../assets/images/logo.png";
+import backButton from "../assets/images/leftArrow.png";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  width: 100%;
+  margin-bottom: 3vh;
+`;
+
+const ButtonContainer = styled.img`
+  width: 5.5vw;
+  height: auto;
+  position: absolute;
+  left: 3vw;
+  cursor: pointer;
+`;
 
 const LogoContainer = styled.img`
   width: 35vw;
   height: auto;
-  margin-bottom: 3vh;
 `;
-
 const ColorContainer = styled.div`
   width: 100%;
   height: 100vh;
@@ -31,9 +48,18 @@ const WhiteContainer = styled.div`
 `;
 
 export default function ChatBackground({ children }) {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
   return (
     <ColorContainer>
-      <LogoContainer src={logo} />
+      <HeaderContainer>
+        <ButtonContainer src={backButton} onClick={handleBackClick} />
+        <LogoContainer src={logo} />
+      </HeaderContainer>
       <WhiteContainer>{children}</WhiteContainer>
     </ColorContainer>
   );
