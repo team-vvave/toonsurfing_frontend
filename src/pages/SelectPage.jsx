e43be6d7699fab98f21d7f59ff48572667f222e3
hypoxisaurea <flickerlight14@naver.com>
2024-07-25 21:00:57 +0900

    [Feat] Add Header

--- src/pages/SelectPage.jsx
+++ src/pages/SelectPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import ColorContainer from "../components/ColorBackground";
+import closeButton from "../assets/images/closeButton.png";
 import thumbnail1 from "../assets/images/thumnails/소녀재판.PNG";
 import thumbnail2 from "../assets/images/thumnails/걔의질투.PNG";
 import thumbnail3 from "../assets/images/thumnails/귀비나리.PNG";
@@ -15,6 +16,21 @@ import thumbnail10 from "../assets/images/thumnails/예명여고.PNG";
 import thumbnail11 from "../assets/images/thumnails/일립예고학생들.PNG";
 import thumbnail12 from "../assets/images/thumnails/자멸기관.PNG";
 import thumbnail13 from "../assets/images/thumnails/처음을줄게.PNG";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  margin-bottom: 6vh;
+`;
+
+const ButtonContainer = styled.img`
+  width: 4vw;
+  height: auto;
+  position: absolute;
+  cursor: pointer;
+  right: 10vw;
+`;
 
 const TextContainer = styled.div`
   font-family: "Pretendard";
@@ -99,8 +115,15 @@ function SelectPage() {
     }
   };
 
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
   return (
     <ColorContainer>
+      <HeaderContainer>
+        <ButtonContainer src={closeButton} onClick={handleBackClick} />
+      </HeaderContainer>
       <TextContainer>어떤 웹툰이 궁금하세요?</TextContainer>
       <ListContainer>
         {items.map((item, index) => (
